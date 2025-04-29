1ac1bbaa4050c3538b4d11cb04f11c400e7e9377
Web samir <websamir2021@gmail.com>
2025-04-29 17:40:26 -0500

    refactor(app/routing) se importaron las rutas de la forma recomendada y moderna

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,20 @@ import { Routes } from '@angular/router';
 import { CreatePaymentComponent } from './features/create-payment/create-payment.component';
 import { TransactionsComponent } from './features/transactions/transactions.component';
 
-
 export const routes: Routes = [
+  {
+    path: 'create-payment',
+    loadComponent: () =>
+      import('./features/create-payment/create-payment.component').then(
+        (m) => m.CreatePaymentComponent
+      ),
+  },
+  {
+    path: 'transactions',
+    loadComponent: () =>
+      import('./features/transactions/transactions.component').then(
+        (m) => m.TransactionsComponent
+      ),
+  },
   { path: '', redirectTo: '/create-payment', pathMatch: 'full' },
-  { path: 'create-payment', component: CreatePaymentComponent },
-  { path: 'transactions', component: TransactionsComponent }
 ];
